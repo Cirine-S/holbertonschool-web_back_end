45b5fd40814b53c1bdfd160f4a36f6fe7f09c562
cirine <1410@holbertonschool.com>
2021-03-23 00:12:57 +0100

    cache

--- 0x03-caching/1-fifo_cache.py
+++ 0x03-caching/1-fifo_cache.py
@@ -1,26 +1,18 @@
 #!/usr/bin/python3
-"""
-FIFOCache module.
-"""
+'''FIFOCache module'''
 
 BaseCaching = __import__('base_caching').BaseCaching
 
 
 class FIFOCache(BaseCaching):
-    """
-        FIFOCache class.
-    """
+    FIFOCache class
 
     def __init__(self):
-        """
-            Initialize.
-        """
+        Initialize
         super().__init__()
 
     def put(self, key, item):
-        """
-            Add an item in the cache.
-        """
+        Add an item in the cache
         if not (key is None or item is None):
             if (
                 len(self.cache_data.keys()) == BaseCaching.MAX_ITEMS and
@@ -34,9 +26,7 @@ class FIFOCache(BaseCaching):
                 self.cache_data[key] = item
 
     def get(self, key):
-        """
-            Get an item by key.
-        """
+        Get an item by key
         if key not in self.cache_data.keys():
             return None
         else:

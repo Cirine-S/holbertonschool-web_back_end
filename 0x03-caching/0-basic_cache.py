8d13eea623841da88cc64910786721e16a80ecd4
cirine <1410@holbertonschool.com>
2021-03-23 00:00:36 +0100

    cache

---
+++ 0x03-caching/0-basic_cache.py
@@ -0,0 +1,27 @@
+
+#!/usr/bin/python3
+"""
+BasicCache module.
+"""
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class BasicCache(BaseCaching):
+    ''''BasicCache class'''''
+
+    def put(self, key, item):
+        """
+            Add an item in the cache.
+        """
+        if not (key is None or item is None):
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """
+            Get an item by key.
+        """
+        if key not in self.cache_data.keys():
+            return None
+        else:
+            return self.cache_data[key]

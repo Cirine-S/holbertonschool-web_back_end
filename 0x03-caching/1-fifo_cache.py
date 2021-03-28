00d570d94d120bcf609d3e5cedcf4b8db6ddf80a
cirine <1410@holbertonschool.com>
2021-03-29 00:28:03 +0100

    fifo discard

--- 0x03-caching/1-fifo_cache.py
+++ 0x03-caching/1-fifo_cache.py
@@ -1,33 +1,24 @@
 #!/usr/bin/python3
-'''FIFOCache module'''
+'''BasicCache module'''
 
 BaseCaching = __import__('base_caching').BaseCaching
 
 
 class FIFOCache(BaseCaching):
-    FIFOCache class
-
-    def __init__(self):
-        Initialize
-        super().__init__()
+    ''''BasicCache class'''''
 
     def put(self, key, item):
-        Add an item in the cache
-        if not (key is None or item is None):
-            if (
-                len(self.cache_data.keys()) == BaseCaching.MAX_ITEMS and
-                (key not in self.cache_data.keys())
-            ):
-                first = sorted(self.cache_data.keys())[0]
-                print('DISCARD: {}'.format(first))
-                del self.cache_data[first]
-                self.cache_data[key] = item
-            else:
-                self.cache_data[key] = item
+        '''Add an item in the cache'''
+        if key is None or item is None:
+            return
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            ItemToDiscard = sorted(self.cache_data)[0]
+            print('DISCARD: {}'.format(ItemToDiscard))
+            self.cache_data.pop(ItemToDiscard)
+        self.cache_data[key] = item
 
     def get(self, key):
-        Get an item by key
-        if key not in self.cache_data.keys():
+        '''Get an item by key'''
+        if key is None or key not in self.cache_data.keys():
             return None
-        else:
-            return self.cache_data[key]
+        return self.cache_data[key]

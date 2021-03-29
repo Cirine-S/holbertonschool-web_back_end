bd3bba592721178f16d3fed0019430e513d08cf0
cirine <1410@holbertonschool.com>
2021-03-29 23:45:35 +0100

    encrypt pwd

---
+++ 0x05-personal_data/encrypt_password.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""
+Module for password Hashing
+0x05-personal_data
+Holberton Web Stack programming Spec ― Back-end
+"""
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """[Encrypting passwords before storing in databases]
+    Args:
+        password ([string]): [paswword to be hashed]
+    Returns:
+        [bcrypt]: [hashed password]
+    """
+
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """[Check valid password]
+    Args:
+        hashed_password (bytes): [hashed password]
+        password (str): [plain text password]
+    Returns:
+        bool: [valid ? true : false]
+    """
+
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)

72baea92ff307db6d55b165fe5ade22e088b299f
cirine <1410@holbertonschool.com>
2021-03-29 20:03:03 +0100

    typing added

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -3,9 +3,13 @@
 Filtered Logger file
 """
 import re
+from typing import List
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(fields: List[str],
+                 redaction: str,
+                 message: str,
+                 separator: str) -> str:
     """returns the log message obfuscated"""
     for field in fields:
         message = re.sub(rf"{field}=.*?{separator}",

4ddefccdf88d6b6bc3ce56389eccbdab1029baef
cirine <1410@holbertonschool.com>
2021-04-05 21:43:20 +0100

    changed doc

--- 0x06-Basic_authentication/api/v1/app.py
+++ 0x06-Basic_authentication/api/v1/app.py
@@ -12,6 +12,13 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
+if os.getenv("AUTH_TYPE") == "auth":
+    from api.v1.auth.auth import Auth
+    auth = Auth()
+elif os.getenv("AUTH_TYPE") == "basic_auth":
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
 
 
 @app.errorhandler(404)
@@ -22,19 +29,38 @@ def not_found(error) -> str:
 
 
 @app.errorhandler(401)
-def unauthorized_access(error) -> str:
+def unauthorized(error) -> str:
     """[ unauthorized request]
     """
     return jsonify({"error": "Unauthorized"}), 401
 
 
 @app.errorhandler(403)
-def forbidden_access(error) -> str:
+def accessForbidden(error) -> str:
     """[ authorized but resource forbidden]
     """
     return jsonify({"error": "Forbidden"}), 403
 
+
+@app.before_request
+def beforeRequestHandler() -> str:
+    """
+    check for user authentication before each request
+    filtering requests
+    """
+
+    x = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
+    if auth is None:
+        return None
+    if auth.require_auth(request.path, x) is False:
+        return None
+    if auth.authorization_header(request) is None:
+        return abort(401)
+    if auth.current_user(request) is None:
+        return abort(403)
+
+
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
-    app.run(host=host, port=port)
+    app.run(host=host, port=port, debug=True)

754c090e1f971f5ac5eb0fe99558c2dad4899206
cirine <1410@holbertonschool.com>
2021-04-05 22:31:00 +0100

    authentification

---
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""
+Module for Authentication
+0x06-Basic_authentication
+holbertonschool-web_back_end
+"""
+
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth():
+    """
+    Auth class that manage the API Authentication
+    """
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """[Check if a specific path need authentication]
+        Args:
+            path (str): [path to check]
+            excluded_paths (List[str]): [list of excluded paths]
+        Returns:
+            bool: [true if authentication need, false otherwise]
+        """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """[return the value of the header request Authorization]
+        Args:
+            request ([type], optional): [description]. Defaults to None.
+        Returns:
+            str: [description]
+        """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """[summary]
+        """
+
+        return None

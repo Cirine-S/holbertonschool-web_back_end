86dfdb9692b59c4817687ca4b57bc918f2c68013
cirine <1410@holbertonschool.com>
2021-04-07 02:15:42 +0100

    extract User credentials

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -51,3 +51,22 @@ class BasicAuth(Auth):
         else:
             return base64.b64decode(
                 base64_authorization_header).decode('utf-8')
+
+    def extract_user_credentials(
+            self, decoded_base64_authorization_header: str) -> (str, str):
+        """[returns the user email and password from the Base64 decoded value]
+
+        Args:
+            self ([type]): [description]
+            str ([type]): [description]
+        Returns:
+            2 values
+        """
+        if decoded_base64_authorization_header is None:
+            return None, None
+        if type(decoded_base64_authorization_header) is not str:
+            return None, None
+        if ":" not in decoded_base64_authorization_header:
+            return None, None
+        emailAndPass = decoded_base64_authorization_header.split(':')
+        return tuple(emailAndPass)

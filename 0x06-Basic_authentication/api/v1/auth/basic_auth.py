ff81f903f18f3df598b67a31caf4b95997d22352
cirine <1410@holbertonschool.com>
2021-04-07 01:26:47 +0100

    decode base64 auth_header

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -24,3 +24,30 @@ class BasicAuth(Auth):
             return None
         else:
             return authorization_header[6:]
+
+    def isBase64(self, s):
+        """try/except method to check base64 encoding"""
+        try:
+            return base64.b64encode(base64.b64decode(s)).decode("utf-8") == s
+        except Exception:
+            return False
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """generates the decoded value of a Base64 string ]
+
+        Args:
+            base64_authorization_header (str): [description]
+
+        Returns:
+            str: [the decoded value of a Base64 string]
+        """
+        if base64_authorization_header is None:
+            return None
+        elif type(base64_authorization_header) is not str:
+            return None
+        elif not self.isBase64(base64_authorization_header):
+            return None
+        else:
+            return base64.b64decode(
+                base64_authorization_header).decode('utf-8')

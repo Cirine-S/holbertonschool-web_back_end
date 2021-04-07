7f0fbb86eebcf0f2602f78b5c43a046e4c083bf8
cirine <1410@holbertonschool.com>
2021-04-07 03:02:01 +0100

    extract User credentials

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -6,6 +6,8 @@ holbertonschool-web_back_end
 """
 from api.v1.auth.auth import Auth
 import base64
+from typing import TypeVar
+from models.user import User
 
 
 class BasicAuth(Auth):
@@ -70,3 +72,19 @@ class BasicAuth(Auth):
             return None, None
         emailAndPass = decoded_base64_authorization_header.split(':')
         return tuple(emailAndPass)
+
+    def user_object_from_credentials(
+            self, user_email: str, user_pwd: str) -> TypeVar('User'):
+        """[returns the User instance based on his email and password]"""
+        if user_email is None or type(user_email) is not str:
+            return None
+        if user_pwd is None or type(user_pwd) is not str:
+            return None
+        try:
+            users = User.search({'email': user_email})
+        except Exception as e:
+            return None
+        for user in users:
+            if user.is_valid_password(user_pwd):
+                return user
+        return None

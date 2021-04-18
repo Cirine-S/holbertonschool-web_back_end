1caf609b8ba12289cbfc571165077095be9c7237
cirine <1410@holbertonschool.com>
2021-04-18 17:24:26 +0100

    excluding login session

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -19,9 +19,6 @@ if os.getenv("AUTH_TYPE") == "basic_auth":
 if os.getenv("AUTH_TYPE") == "auth":
     from api.v1.auth.auth import Auth
     auth = Auth()
-if os.getenv("AUTH_TYPE") == "session_auth":
-    from api.v1.auth.session_auth import SessionAuth
-    auth = SessionAuth()
 
 
 @app.before_request
@@ -31,7 +28,8 @@ def before_request_handler() -> str:
     if auth is None:
         return None
     excluded_paths = ['/api/v1/status/',
-                      '/api/v1/unauthorized/', '/api/v1/forbidden/']
+                      '/api/v1/unauthorized/', '/api/v1/forbidden/',
+                      '/api/v1/auth_session/login/']
 
     if auth.require_auth(request.path, excluded_paths) is False:
         return None
@@ -39,7 +37,8 @@ def before_request_handler() -> str:
         return abort(401)
     if auth.current_user(request) is None:
         return abort(403)
-    request.current_user = auth.current_user(request)
+    if auth.authorization_header(request) and auth.session_cookie(request):
+        return None, abort(401)
 
 
 @app.errorhandler(404)
@@ -64,6 +63,6 @@ def forbidden_access(error) -> str:
 
 
 if __name__ == "__main__":
-    host = getenv("API_HOST")
-    port = getenv("API_PORT")
+    host = getenv("API_HOST", "0.0.0.0")
+    port = getenv("API_PORT", "5000")
     app.run(host=host, port=port)

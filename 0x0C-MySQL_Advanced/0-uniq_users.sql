1d6ebcbd78811d2e4686df443e1dba0a693ec55f
cirine <1410@holbertonschool.com>
2021-04-29 23:34:37 +0100

    if not exists cond

--- 0x0C-MySQL_Advanced/0-uniq_users.sql
+++ 0x0C-MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,6 @@
 -- SQL script that creates a table users
 
-CREATE users(
+CREATE TABLE IF NOT EXISTS users(
     id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255)

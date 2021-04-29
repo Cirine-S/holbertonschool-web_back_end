daea899652c077779ee4460c5df323b29eb731c9
cirine <1410@holbertonschool.com>
2021-04-29 23:58:02 +0100

    TRIGGER CREATING

---
+++ 0x0C-MySQL_Advanced/4-store.sql
@@ -0,0 +1,7 @@
+-- Creates a trigger that decreases the quantity of an item after adding a new order
+
+CREATE TRIGGER desc_items_after_order_trigger
+AFTER INSERT ON orders
+FOR EACH ROW UPDATE items
+SET quantity = quantity - NEW.number
+WHERE items.name = NEW.item_name;

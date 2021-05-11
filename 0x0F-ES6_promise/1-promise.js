f94077d66c855495e7dd39f3dfe09396f62b7fc0
cirine <1410@holbertonschool.com>
2021-05-11 22:15:44 +0100

    promise resolve & reject handling

--- 0x0F-ES6_promise/1-promise.js
+++ 0x0F-ES6_promise/1-promise.js
@@ -1,10 +1,9 @@
 export default function getFullResponseFromAPI(success) {
-    const myPromise = new Promise((resolve) => {
-        if (success){
-            resolve({status: 200, body: 'Success'});
-        } else {
-        reject(new Error('The fake API is not working currently'))
-        }
-      });
-      return myPromise;
+  return new Promise((resolve, reject) => {
+    if (success) {
+      resolve({ status: 200, body: 'Success' });
+    } else {
+      reject(new Error('The fake API is not working currently'));
+    }
+  });
 }

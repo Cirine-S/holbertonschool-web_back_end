ac93bf0bbc5d0e2b09895fb15771b0a036e58757
cirine <1410@holbertonschool.com>
2021-05-11 22:30:44 +0100

    got a response from api

---
+++ 0x0F-ES6_promise/3-all.js
@@ -0,0 +1,12 @@
+export default function handleProfileSignup() {
+  let msg = '';
+  return Promise.all([
+    uploadPhoto(),
+    createUser(),
+  ]).then((data) => {
+    msg += `${data[0].body} ${data[1].firstName} ${data[1].lastName}`;
+    console.log(msg);
+  }).catch(() => {
+    console.log('Signup system offline');
+  });
+}

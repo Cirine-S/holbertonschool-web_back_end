e1b12afe337faab9910d1405346c0986ec4a8f9c
cirine <1410@holbertonschool.com>
2021-05-11 22:27:39 +0100

    got a response from api

--- 0x0F-ES6_promise/2-then.js
+++ 0x0F-ES6_promise/2-then.js
@@ -1,11 +1,7 @@
 export default function handleResponseFromAPI(promise) {
-  return new Promise((resolve, reject) => {
-    console.log('Got a response from the API')
-    if (promise) {
-      resolve({ status: 200, body: 'Success' });
-    } else {
-      reject(new Error());
-    }
-
-  });
+  console.log('Got a response from the API');
+  return promise.then(() => ({
+    status: 200,
+    body: 'success',
+  })).catch(() => new Error());
 }

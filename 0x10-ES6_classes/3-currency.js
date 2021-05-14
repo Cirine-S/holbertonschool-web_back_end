2870df62b704521378e08888837fb0d8d7b1f5d8
cirine <1410@holbertonschool.com>
2021-05-14 23:19:56 +0100

    display currency

--- 0x10-ES6_classes/3-currency.js
+++ 0x10-ES6_classes/3-currency.js
@@ -1,26 +1,26 @@
 export default class HolbertonCourse {
-    constructor(code, name) {
-      this._code = code;
-      this._name = name;
-    }
-  
-    get code() {
-      return this._code;
-    }
-  
-    set code(code) {
-      this._code = code;
-    }
-  
-    get name() {
-      return this._name;
-    }
-  
-    set name(name) {
-      this._name = name;
-    }
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
+  }
 
-    displayFullCurrency() {
-        return this._name + " (" + this._code + ")";
-    }
+  get code() {
+    return this._code;
+  }
+
+  set code(code) {
+    this._code = code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(name) {
+    this._name = name;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
 }

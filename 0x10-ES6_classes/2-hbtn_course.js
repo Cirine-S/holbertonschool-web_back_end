c3d261a57177736d7fb6271b185eceed423fe1ea
cirine <1410@holbertonschool.com>
2021-05-14 22:03:11 +0100

    classrooms filled

---
+++ 0x10-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,42 @@
+export default class HolbertonCourse {
+  Constructor(name, length, students) {
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(name) {
+    if (typeof name !== 'string') {
+      throw TypeError('Name must be a string');
+    }
+    this._name = name;
+  }
+
+  get length() {
+    return this._length();
+  }
+
+  set length(length) {
+    if (typeof length !== 'number') {
+      throw TypeError('Length must be a number');
+    }
+    this._length = length;
+  }
+
+  get students() {
+    return this._students();
+  }
+
+  set students(students) {
+    students.forEach((student) => {
+      if (typeof student !== 'string') {
+        throw TypeError('Students must be an array of strings');
+      }
+    });
+    this._students = students;
+  }
+}

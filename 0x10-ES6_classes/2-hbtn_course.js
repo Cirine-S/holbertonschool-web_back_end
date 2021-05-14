45c9d99e94f0ab4bd3ad857247d953b2ec014cdc
cirine <1410@holbertonschool.com>
2021-05-14 23:08:32 +0100

    class

--- 0x10-ES6_classes/2-hbtn_course.js
+++ 0x10-ES6_classes/2-hbtn_course.js
@@ -5,37 +5,38 @@ export default class HolbertonCourse {
     this.students = students;
   }
 
-  set name(name) {
-    if (typeof name !== 'string') {
-      throw new TypeError('Name must be a string');
-    }
-    this._name = name;
-  }
-
   get name() {
     return this._name;
   }
 
+  set name(name) {
+    if (typeof name !== 'string') {
+      throw TypeError('Name must be a string');
+    }
+    this._name = name;
+  }
+
+  get length() {
+    return this._length();
+  }
+
   set length(length) {
-    console.log('heyeeeyeyeyey');
     if (typeof length !== 'number') {
-      throw new TypeError('Length must be a number');
+      throw TypeError('Length must be a number');
     }
     this._length = length;
   }
 
-  get length() {
-    return this._length;
+  get students() {
+    return this._students;
   }
 
   set students(students) {
-    if (!Array.isArray(students) || students.some((s) => typeof s !== 'string')) {
-      throw new TypeError('Students must be an array of strings');
-    }
+    students.forEach((student) => {
+      if (typeof student !== 'string') {
+        throw TypeError('Students must be an array of strings');
+      }
+    });
     this._students = students;
-  }
-
-  get students() {
-    return this._students;
   }
 }

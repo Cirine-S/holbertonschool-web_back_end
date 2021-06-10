a256827c3070fdf16cd29a026611e9d69844b878
cirine <1410@holbertonschool.com>
2021-06-10 17:40:24 +0100

    resolve all promises

--- 0x0F-ES6_promise/3-all.js
+++ 0x0F-ES6_promise/3-all.js
@@ -1,14 +1,12 @@
-import uploadPhoto from "./utils";
-import createUser from "./utils";
+import { uploadPhoto, createUser } from './utils';
+
 export default function handleProfileSignup() {
-  let msg = '';
-  return Promise.all([
-    uploadPhoto,
-    createUser,
-  ]).then((data) => {
-    msg += `${data[0].body} ${data[1].firstName} ${data[1].lastName}`;
-    console.log(msg);
-  }).catch(() => {
-    console.log('Signup system offline');
-  });
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((values) => {
+      const { body } = values[0];
+      const { firstName } = values[1];
+      const { lastName } = values[1];
+      console.log(`${body} ${firstName} ${lastName}`);
+    })
+    .catch(() => console.log('Signup system offline'));
 }

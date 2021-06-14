dcb5700e9a2624afeeb84ecc06b26a85ed551961
cirine <1410@holbertonschool.com>
2021-06-14 23:41:52 +0100

    pricing

---
+++ 0x10-ES6_classes/4-pricing.js
@@ -0,0 +1,33 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(amount) {
+    this._amount = amount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(currency) {
+    this._currency = currency;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency._name} (${this.currency._code})`;
+  }
+
+  convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
+

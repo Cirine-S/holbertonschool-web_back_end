1164f4478d0769f1cfc9f6554d9ab98d6c3481a8
cirine <1410@holbertonschool.com>
2021-06-17 14:35:39 +0100

    refine

--- 0x12-Node_JS_basic/6-http_express.js
+++ 0x12-Node_JS_basic/6-http_express.js
@@ -2,14 +2,11 @@ const express = require('express');
 
 const app = express();
 const port = 1245;
-const host = '127.0.0.1';
+const hostname = '127.0.0.1';
 
 app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
 
-app.listen(port, () => {
-  console.log(`Server listening at http://${host}:${port}`);
-});
-
+app.listen(port, hostname);
 module.exports = app;
